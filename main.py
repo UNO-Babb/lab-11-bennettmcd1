0d58afa1909dd6f17ca8b91ab54b669f2e4deb40
bennettmcd1 <bennettmcdonald@unomaha.edu>
2025-04-30 14:42:03 +0000

    work

--- main.py
+++ main.py
@@ -1,4 +1,68 @@
 #GroceryStoreSim.py
-#Name:
-#Date:
-#Assignment:
+#Name: Bennett McDonald
+#Date: 4/30/25
+#Assignment: grocery sim
+import simpy
+import random
+
+eventLog = []
+waitingShoppers = []
+idleTime = 0
+
+def shopper(env, id):
+    arrive = env.now
+    items = random.randint(5, 20)
+    shoppingTime = items // 2
+    yield env.timeout(shoppingTime)
+    waitingShoppers.append((id, items, arrive, env.now))
+
+def checker(env):
+    global idleTime
+    while True:
+        while len(waitingShoppers) == 0:
+            idleTime += 1
+            yield env.timeout(1)
+
+        customer = waitingShoppers.pop(0)
+        items = customer[1]
+        checkoutTime = items // 10 + 1
+        yield env.timeout(checkoutTime)
+
+        eventLog.append((customer[0], customer[1], customer[2], customer[3], env.now))
+
+def customerArrival(env):
+    customerNumber = 0
+    while True:
+        customerNumber += 1
+        env.process(shopper(env, customerNumber))
+        yield env.timeout(2)
+
+def processResults():
+    totalWait = 0
+    totalShoppers = 0
+
+    for e in eventLog:
+        waitTime = e[4] - e[3]
+        totalWait += waitTime
+        totalShoppers += 1
+
+    avgWait = totalWait / totalShoppers if totalShoppers > 0 else 0
+
+    print("The average wait time was %.2f minutes." % avgWait)
+    print("The total idle time was %d minutes" % idleTime)
+
+def main():
+    numberCheckers = 5
+
+    env = simpy.Environment()
+
+    env.process(customerArrival(env))
+    for i in range(numberCheckers):
+        env.process(checker(env))
+
+    env.run(until=180)
+    print(f"Total number of shoppers who completed shopping: {len(waitingShoppers)}")
+    processResults()
+
+if __name__ == '__main__':
+    main()
